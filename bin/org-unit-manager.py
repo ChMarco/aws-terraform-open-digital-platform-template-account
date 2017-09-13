a9073ca3ce688084feb2b83cefe54941836c8c22
Sion Smith <sion.smith@gmail.com>
2017-09-13 12:30:53 +0200

    - managing policies implemented - fixed problem with enabling policy on root org - policy documents defined as YAML

--- bin/org-unit-manager.py
+++ bin/org-unit-manager.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+import json
 
 import pprint
 
 import boto3
-from utils import get_deployed_ou, get_deployed_policies, get_deployed_accounts, get_logger, get_root_org_id, \
-    validate_spec_file
+from utils import *
 
 
 def enable_policy_type_in_root(org_client, root_id):
@@ -16,6 +16,82 @@ def enable_policy_type_in_root(org_client, root_id):
     if (p_type['Type'] == 'SERVICE_CONTROL_POLICY' and p_type['Status'] != 'ENABLED'):
         org_client.enable_policy_type(RootId=root_id, PolicyType='SERVICE_CONTROL_POLICY')
 
+def validate_accounts_unique_in_org(log, root_spec):
+    """
+    Ensure accounts are unique across org
+    """
+    # recursively build mapping of accounts to ou_names
+    def map_accounts(spec, account_map={}):
+        if 'Accounts' in spec and spec['Accounts']:
+            for account in spec['Accounts']:
+                if account in account_map:
+                    account_map[account].append(spec['Name'])
+                else:
+                    account_map[account] = [(spec['Name'])]
+        if 'Child_OU' in spec and spec['Child_OU']:
+            for child_spec in spec['Child_OU']:
+                map_accounts(child_spec, account_map)
+        return account_map
+    # find accounts set to more than one OU
+    unique = True
+    for account, ou in map_accounts(root_spec).items():
+        if len(ou) > 1:
+            log.error("Account '%s' set in multiple OU: %s" % (account, ou))
+            unique = False
+    if not unique:
+        log.critical("Invalid org_spec: Do not assign accounts to multiple "
+                "Organizatinal Units")
+        print("Invalid org_spec... ")
+        sys.exit(1)
+
+def manage_policies(org_client, log, deployed, org_spec):
+    """
+    Manage Service Control Policies in the AWS Organization.  Make updates
+    according to the sc_policies specification.  Do not touch
+    the default policy.  Do not delete an attached policy.
+    """
+    for p_spec in org_spec['sc_policies']:
+        policy_name = p_spec['Name']
+        log.debug("considering sc_policy: %s" % policy_name)
+        # dont touch default policy
+        if policy_name == org_spec['default_policy']:
+            continue
+        policy = lookup(deployed['policies'], 'Name', policy_name)
+        # delete existing sc_policy
+        if ensure_absent(p_spec):
+            if policy:
+                log.info("Deleting policy '%s'" % (policy_name))
+                # dont delete attached policy
+                if org_client.list_targets_for_policy( PolicyId=policy['Id'])['Targets']:
+                    log.error("Cannot delete policy '%s'. Still attached to OU" %
+                            policy_name)
+                else:
+                    org_client.delete_policy(PolicyId=policy['Id'])
+            continue
+        # create or update sc_policy
+        statement = dict(Effect=p_spec['Effect'], Action=p_spec['Actions'], Resource='*')
+        policy_doc = json.dumps(dict(Version='2012-10-17', Statement=[statement]))
+        log.debug("spec sc_policy_doc: %s" % policy_doc)
+        # create new policy
+        if not policy:
+            log.info("Creating policy '%s'" % policy_name)
+            org_client.create_policy(
+                    Content=policy_doc,
+                    Description=p_spec['Description'],
+                    Name=p_spec['Name'],
+                    Type='SERVICE_CONTROL_POLICY')
+        # check for policy updates
+        else:
+            deployed_policy_doc = json.dumps(json.loads(org_client.describe_policy(
+                    PolicyId=policy['Id'])['Policy']['Content']))
+            log.debug("real sc_policy_doc: %s" % deployed_policy_doc)
+            if (p_spec['Description'] != policy['Description']
+                or policy_doc != deployed_policy_doc):
+                log.info("Updating policy '%s'" % policy_name)
+                org_client.update_policy(
+                        PolicyId=policy['Id'],
+                        Content=policy_doc,
+                        Description=p_spec['Description'],)
 
 def main():
     log = get_logger('--boto-log')
@@ -37,8 +113,27 @@ def main():
     print("Validating Organization spec file")
     org_spec = validate_spec_file(log, '../config/org-spec.yaml', 'org_spec')
     print("Spec Valid...")
-    #TODO: do i need this?
-    #enable_policy_type_in_root(org_client, root_id)
+    #TODO: do i need this and also should we check accounts exist?
+    enable_policy_type_in_root(org_client, root_id)
+    validate_master_id(org_client, org_spec)
+    root_spec = lookup(org_spec['organizational_units'], 'Name', 'root')
+    validate_accounts_unique_in_org(log, root_spec)
+    managed = dict(
+            accounts = search_spec(root_spec, 'Accounts', 'Child_OU'),
+            ou = search_spec(root_spec, 'Name', 'Child_OU'),
+            policies = [p['Name'] for p in org_spec['sc_policies']])
+    # ensure default_policy is considered 'managed'
+    if org_spec['default_policy'] not in managed['policies']:
+        managed['policies'].append(org_spec['default_policy'])
+
+    ###################### POLICY CRUD ######################
+    #########################################################
+    # all information is present now preform CRUD operations on policies
+    manage_policies(org_client, log, deployed, org_spec)
+    # rescan deployed policies
+    deployed['policies'] = get_deployed_policies(org_client)
+
+
 
 if __name__ == "__main__":
     main()
--- bin/utils.py
+++ bin/utils.py
@@ -209,3 +209,66 @@ def validate_spec(log, validation_patterns, pattern_name, spec):
                         valid_spec = False
                         continue
     return valid_spec
+
+def lookup(dlist, lkey, lvalue, rkey=None):
+    """
+    Use a known key:value pair to lookup a dictionary in a list of
+    dictionaries.  Return the dictonary or None.  If rkey is provided,
+    return the value referenced by rkey or None.  If more than one
+    dict matches, raise an error.
+    args:
+        dlist:   lookup table -  a list of dictionaries
+        lkey:    name of key to use as lookup criteria
+        lvalue:  value to use as lookup criteria
+        rkey:    (optional) name of key referencing a value to return
+    """
+    items = [d for d in dlist
+             if lkey in d
+             and d[lkey] == lvalue]
+    if not items:
+        return None
+    if len(items) > 1:
+        raise RuntimeError(
+            "Data Error: lkey:lvalue lookup matches multiple items in dlist"
+        )
+    if rkey:
+        if rkey in items[0]:
+            return items[0][rkey]
+        return None
+    return items[0]
+
+def validate_master_id(org_client, spec):
+    """
+    Don't mangle the wrong org by accident
+    """
+    master_account_id = org_client.describe_organization(
+      )['Organization']['MasterAccountId']
+    if master_account_id != spec['master_account_id']:
+        errmsg = ("The Organization Master Account Id '%s' does not match the "
+                "'master_account_id' set in the spec-file" % master_account_id)
+        raise RuntimeError(errmsg)
+    return
+
+def search_spec(spec, search_key, recurse_key):
+    """
+    Recursively scans spec structure and returns a list of values
+    keyed with 'search_key' or and empty list.  Assumes values
+    are either list or str.
+    """
+    value = []
+    if search_key in spec and spec[search_key]:
+        if isinstance(spec[search_key], str):
+            value.append(spec[search_key])
+        else:
+            value += spec[search_key]
+    if recurse_key in spec and spec[recurse_key]:
+        for child_spec in spec[recurse_key]:
+            value += search_spec(child_spec, search_key, recurse_key)
+    return sorted(value)
+
+def ensure_absent(spec):
+    """
+    test if an 'Ensure' key is set to absent in dictionary 'spec'
+    """
+    if 'Ensure' in spec and spec['Ensure'] == 'absent': return True
+    return False
