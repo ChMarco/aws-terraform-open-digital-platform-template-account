5d148801c32a3dbd49ea62c779a58e47e6124f0b
Sion Smith <sion.smith@gmail.com>
2017-10-11 09:44:47 +0200

    added create account / account status.

--- bin/account-manager.py
+++ bin/account-manager.py
@@ -25,8 +25,24 @@ Options:
 
 import boto3
 from docopt import docopt
+import time
 from utils import *
 
+def scan_created_accounts(log, org_client):
+    """
+    Query AWS Organization for accounts with creation status of 'SUCCEEDED'.
+    Returns a list of dictionary.
+    """
+    log.debug('running')
+    status = org_client.list_create_account_status(States=['SUCCEEDED'])
+    created_accounts = status['CreateAccountStatuses']
+    while 'NextToken' in status and status['NextToken']:
+        log.debug("NextToken: %s" % status['NextToken'])
+        status = org_client.list_create_account_status(
+                States=['SUCCEEDED'],
+                NextToken=status['NextToken'])
+        created_accounts += status['CreateAccountStatuses']
+    return created_accounts
 
 def display_provisioned_accounts(log, deployed_accounts):
     """
@@ -41,14 +57,64 @@ def display_provisioned_accounts(log, deployed_accounts):
         spacer = ' ' * (24 - len(a_name))
         log.info("%s%s%s\t\t%s" % (a_name, spacer, a_id, a_email))
 
+def create_accounts(org_client, args, log, deployed_accounts, account_spec):
+    """
+    Compare deployed_accounts to list of accounts in the accounts spec.
+    Create accounts not found in deployed_accounts.
+    """
+    for a_spec in account_spec['accounts']:
+        if not lookup(deployed_accounts, 'Name', a_spec['Name'],):
+            # check if it is still being provisioned
+            created_accounts = scan_created_accounts(log, org_client)
+            if lookup(created_accounts, 'AccountName', a_spec['Name']):
+                log.warn("New account '%s' is not yet available" %
+                        a_spec['Name'])
+                break
+            # create a new account
+            if 'Email' in a_spec and a_spec['Email']:
+                email_addr = a_spec['Email']
+            else:
+                email_addr = '%s@%s' % (a_spec['Name'], account_spec['default_domain'])
+            log.info("Creating account '%s'" % (a_spec['Name']))
+            log.debug('account email: %s' % email_addr)
+            if args['--exec']:
+                new_account = org_client.create_account(
+                        AccountName=a_spec['Name'], Email=email_addr)
+                create_id = new_account['CreateAccountStatus']['Id']
+                log.info("CreateAccountStatus Id: %s" % (create_id))
+                # validate creation status
+                counter = 0
+                maxtries = 5
+                while counter < maxtries:
+                    creation = org_client.describe_create_account_status(
+                            CreateAccountRequestId=create_id
+                            )['CreateAccountStatus']
+                    if creation['State'] == 'IN_PROGRESS':
+                        time.sleep(5)
+                        log.info("Account creation in progress for '%s'" %
+                                a_spec['Name'])
+                    elif creation['State'] == 'SUCCEEDED':
+                        log.info("Account creation succeeded")
+                        break
+                    elif creation['State'] == 'FAILED':
+                        log.error("Account creation failed: %s" %
+                                creation['FailureReason'])
+                        break
+                    counter += 1
+                if counter == maxtries and creation['State'] == 'IN_PROGRESS':
+                     log.warn("Account creation still pending. Moving on!")
+
+def unmanaged_accounts(log, deployed_accounts, account_spec):
+    # compare accounts that in spec file which that are created under root.
+    deployed_account_names = [a['Name'] for a in deployed_accounts]
+    spec_account_names = [a['Name'] for a in account_spec['accounts']]
+    log.debug('deployed_account_names: %s' % deployed_account_names)
+    log.debug('spec_account_names: %s' % spec_account_names)
+    return [a for a in deployed_account_names if a not in spec_account_names]
+
 def main():
-    args = docopt(__doc__)
-    logging_level = logging.INFO
-    if args['--debug']:
-        logging_level = logging.DEBUG
-
-    log = get_logger(logging_level, os.path.basename(__file__).split('.')[0])
-
+    args = docopt(__doc__, version='1.0')
+    log = get_logger(args, os.path.basename(__file__).split('.')[0])
 
     #create the client
     org_client = boto3.client('organizations')
@@ -62,6 +128,12 @@ def main():
 
     if args['report']:
         display_provisioned_accounts(log, deployed_accounts)
+
+    if args['create']:
+        create_accounts(org_client, args, log, deployed_accounts, account_spec)
+        unmanaged = unmanaged_accounts(log, deployed_accounts, account_spec)
+        if unmanaged:
+            log.warn("Unmanaged accounts in Org: %s" % (', '.join(unmanaged)))
 
 if __name__ == "__main__":
     main()
